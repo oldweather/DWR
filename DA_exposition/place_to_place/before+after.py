45c25c93402218501e22a0bc50409febb3a354a0
Philip Brohan <philip@brohan.org>
2018-03-19 17:34:05 +0000

    Writing paper and preparing DA exposition plots.

---
+++ DA_exposition/place_to_place/before+after.py
@@ -0,0 +1,130 @@
+# UK region weather plot 
+# Relationship between Fort William and Stornoway
+
+import math
+import datetime
+import numpy
+import collections
+
+import iris
+import iris.analysis
+
+import matplotlib
+from matplotlib.backends.backend_agg import \
+                 FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.patches import Circle
+
+import Meteorographica.data.twcr as twcr
+
+import DWR
+import DIYA
+
+import sklearn.linear_model
+
+# Date to show 
+year=1903
+month=10
+day=22
+hour=18
+dte=datetime.datetime(year,month,day,hour)
+
+# Landscape page
+fig=Figure(figsize=(22,11),  # Width, Height (inches)
+           dpi=100,
+           facecolor=(0.88,0.88,0.88,1),
+           edgecolor=None,
+           linewidth=0.0,
+           frameon=False,
+           subplotpars=None,
+           tight_layout=None)
+canvas=FigureCanvas(fig)
+font = {'family' : 'sans-serif',
+        'sans-serif' : 'Arial',
+        'weight' : 'normal',
+        'size'   : 16}
+matplotlib.rc('font', **font)
+
+# Get the DWR observations within +- 2 hours
+obs=DWR.load_observations('prmsl',
+                          dte-datetime.timedelta(hours=2),
+                          dte+datetime.timedelta(hours=2))
+# Discard everthing except Fort William
+obs=obs[obs.name=='FORTWILLIAM']
+
+# load the pressures
+prmsl=twcr.load('prmsl',year,month,day,hour,
+                             version='2c')
+
+# Compare with Stornoway
+target_lat= 58.21
+target_lon= -6.38
+
+# Before scatter plot
+
+ax_scp=fig.add_axes([0.05,0.05,0.39,0.89])
+
+# pressure range
+extent=[987,999]
+
+# x-axis
+ax_scp.set_xlim(extent)
+ax_scp.set_xlabel('MSLP at Fort William (hPa)')
+# y-axis
+ax_scp.set_ylim(extent)
+ax_scp.set_ylabel('Original MSLP at Stornoway (hPa)')
+
+# Ensemble values at Fort William
+interpolator = iris.analysis.Linear().interpolator(prmsl, 
+                                   ['latitude', 'longitude'])
+latlon=DWR.get_station_location(obs,'FORTWILLIAM')
+ens_FW=interpolator([latlon['latitude'],
+                     latlon['longitude']])
+# And at Stornoway
+ens_ST=interpolator([numpy.array(target_lat),
+                     numpy.array(target_lon)])
+
+ax_scp.scatter(ens_FW.data/100,ens_ST.data/100,
+            500, # Point size
+            'blue', # Color
+            marker='.',
+            edgecolors='face',
+            linewidths=0.0,
+            alpha=1,
+            zorder=5)
+
+# Plot the observed FW value
+fwi=DWR.at_station_and_time(obs,'FORTWILLIAM',dte)
+ax_scp.plot((fwi,fwi),extent,color='red',lw=3)
+
+# Fit a linear model
+model=sklearn.linear_model.LinearRegression()
+model.fit(ens_FW.data.reshape(-1,1),ens_ST.data)
+pre=model.predict(numpy.array((extent[0]*100,extent[1]*100)).reshape(-1,1))
+ax_scp.plot(extent,pre/100,color='black',lw=3)
+
+ax_scp2=fig.add_axes([0.55,0.05,0.39,0.89])
+
+# pressure range
+extent=[987,999]
+
+# x-axis
+ax_scp2.set_xlim(extent)
+ax_scp2.set_xlabel('original MSLP at Fort William (hPa)')
+# y-axis
+ax_scp2.set_ylim(extent)
+ax_scp2.set_ylabel('Adjusted MSLP at Stornoway (hPa)')
+
+
+ax_scp2.scatter(ens_FW.data/100,ens_ST.data/100,
+            500, # Point size
+            'blue', # Color
+            marker='.',
+            edgecolors='face',
+            linewidths=0.0,
+            alpha=1,
+            zorder=5)
+
+# Output as png
+fig.savefig('before+after_%04d%02d%02d%02d.png' % 
+                                    (year,month,day,hour))
--- docs/da_for_ds.rst
+++ docs/da_for_ds.rst
@@ -1,3 +1,36 @@
 Meteorological Data Assimilation for Data Scientists
 ====================================================
 
+Suppose we have an uncertain estimate of the weather (MSLP) at some point in the past, perhaps `from a reanalysis <reanalysis_and_uncertainty.html>`_, and then we get some new information, in the form of a station observation from that date and time. How can we use that new station observation to improve the MSLP estimate?
+
+|
+
+.. figure:: ../DA_exposition/one_station/With_FW_1903102218.png
+   :width: 650px
+   :align: center
+   :figwidth: 700px
+
+   MSLP Contours for 20CR2c for October 22nd, 1903 (at 6pm).
+
+   `Spaghetti-contour plot <analyses/spaghetti_contour/spaghetti_contour.html>`_ of mean-sea-level pressure, and the location of the Fort William station (red dot), where we have new observational data from `Weather Rescue <http://weatherrescue.org>`_
+
+|
+
+We can edit the reanalysis ensemble at the location of the new observation - setting all ensemble members to the observed value, but we would like to do more than this, to use information from the observation at nearby locations as well. To decide how to modify the mslp at, say, Stornoway and London, in response to an observation at Fort William, we need to know mslp variability in those places relates to weather variability at the location of the observation. However, we can estimate this directly from the ensemble:
+
+|
+
+.. figure:: ../DA_exposition/place_to_place/lr+contour_1903102218.png
+   :width: 650px
+   :align: center
+   :figwidth: 700px
+
+   MSLP at Fort William, Stornoway, and London (October 22nd, 1903 at 6pm).
+
+   The red dot marks Fort William, the blue Stornoway, and the black London. Scatter plots show relationship between their mslp, in the 20CR2c ensemble for this particular point in time.
+
+|
+
+In the 20CR2c ensemble, at this point in time, the mslp at Stornoway is highly correlated with that at Fort William, so an observation at Fort William is telling us a lot about the mslp at Stornoway, and we should move the mslp estimates at Stornoway in response to the observation in much the same way as we move the estimates at Fort William. At London, on the other hand, the mslp is almost uncorrelated with that at Fort William, so an observation at Fort William is telling us little about the mslp at London, and we should leave the ensemble at London unchanged whatever the observation at Fort William is.
+
+We can formalise this by building a model:
